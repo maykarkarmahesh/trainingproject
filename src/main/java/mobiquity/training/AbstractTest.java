9ce76a967cef6137b62c2949f6fdc89873a5e395
Mahesh Maykarkar <mmaykarkar@mobiquityinc.com>
2015-07-25 13:10:29 +0530

    Abstract class changes

---
+++ src/main/java/mobiquity/training/AbstractTest.java
@@ -0,0 +1,18 @@
+package mobiquity.training;
+
+public abstract class AbstractTest {
+
+//	private String name;
+//	private static int a = 10;
+//
+//	public abstract void show();
+//
+//	public AbstractTest(String name) {
+//		super();
+//		this.name = name;
+//	}
+//
+//	public static void write() {
+//
+//	}
+}
